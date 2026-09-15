c0463c3c59de0f26ab1bf8a7b71fb4923bc7dd83
mwmosca <mwmosca93@gmail.com>
2026-09-14 23:01:11 -0400

    modify_card_name

--- scryfall_utils.py
+++ scryfall_utils.py
@@ -6,6 +6,7 @@ COOLDOWN_MIN   = 0.5     # Limit requests to 2/second on average
 HOSTNAME       = 'api.scryfall.com'
 SCHEME         = 'https' # API requests are only served over HTTPS, using TLS 1.2 or better. Requests will not be honored over plaintext HTTP.
 
+
 def card_collection_post_request(
         identifiers: list,       # An array of JSON objects, each one a card identifier. 
         user_agent:  str,        # REQUIRED - must be accurate to your usage context
@@ -39,3 +40,24 @@ def card_collection_post_request(
             sleep(COOLDOWN_MIN) # Don't exceed the rate limit!!!
     print('Done! ')
     return cards, missing
+
+
+# *****************************************************************
+# Modify the card name to be more descriptive to the human eye.
+# *****************************************************************
+def modify_card_name(card: dict) -> str:
+    mods = ''
+    if card['promo']:                                               mods += 'Promo, '
+    if card['full_art'] and card['set'] != 'ust':                   mods += 'Full Art, '
+    if card['textless']:                                            mods += 'Textless, '
+    if card['released_at'][:4] > '2000' and card['frame'] < '2000': mods += 'Retro Frame, '
+    try:
+        if 'extendedart' in card['frame_effects']:                  mods += 'Extended Art, '
+        if 'showcase'    in card['frame_effects']:                  mods += 'Showcase, '
+    except KeyError:
+        print(f'Card {card["id"]} does not have frame effects.')
+
+    if len(mods) > 0:
+        return f'{card["name"]} ({mods[:len(mods) - 2]})'
+    else:
+        return card['name']
