24fc4b9a6a1955bcb6a91b1cee8925f02299c94e
mwmosca <mwmosca93@gmail.com>
2026-09-14 23:02:20 -0400

    use modify_card_name

--- create_card_report.py
+++ create_card_report.py
@@ -2,7 +2,7 @@ import csv
 from dotenv import load_dotenv
 from os import getenv
 from pathlib import Path
-from scryfall_utils import card_collection_post_request
+from scryfall_utils import card_collection_post_request, modify_card_name
 
 def main() -> None:
     load_dotenv()
@@ -26,19 +26,7 @@ def main() -> None:
 
     # Modify the card name to be more descriptive to the human eye. **************************
     for c in cards:
-        mods = ''
-        if c['promo']:                                            mods += 'Promo, '
-        if c['full_art'] and c['set'] != 'ust':                   mods += 'Full Art, '
-        if c['textless']:                                         mods += 'Textless, '
-        if c['released_at'][:4] > '2000' and c['frame'] < '2000': mods += 'Retro Frame, '
-        try:
-            if 'extendedart' in c['frame_effects']:               mods += 'Extended Art, '
-            if 'showcase'    in c['frame_effects']:               mods += 'Showcase, '
-        except KeyError: 
-            print(f'Card {c["id"]} does not have frame effects.')
-
-        if len(mods) > 0: c['name_'] = f'{c["name"]} ({mods[:len(mods) - 2]})'
-        else:             c['name_'] = c['name']
+        c['name_'] = modify_card_name(c)
 
     # Write local reports ********************************************************************************************************
     with open(Path(PROJECT_DIRECTORY, 'reports', 'card_report.csv'), mode='w', newline='', encoding='utf-8-sig') as f:
